26a8f9555a639be2e749842a5a21aad6a95c0ca7
Ziffcry <nazif2996@gmail.com>
2025-07-05 00:57:36 +0800

    Order CRUD
    in this commit the order CRUD operation fully functioning

--- backend/includes/sidebar.php
+++ backend/includes/sidebar.php
@@ -38,6 +38,14 @@
                         <span class="nav-link-text ms-1">Report Page</span>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white <?php echo ($current_page == 'orders.php') ? 'bg-gradient-primary' : ''; ?>" href="orders.php">
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="material-symbols-rounded opacity-10">table_view</i>
+                        </div>
+                        <span class="nav-link-text ms-1">Order Page</span>
+                    </a>
+                </li>
             </ul>
         </div>
 
